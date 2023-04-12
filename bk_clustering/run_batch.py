fe203f23542645a73e11a9750ee77fbe827458a1
Ivan Reznikov <ivanreznikov@gmail.com>
2023-04-13 02:42:50 +0400

    updating results

--- bk_clustering/run_batch.py
+++ bk_clustering/run_batch.py
@@ -1,16 +1,14 @@
-import numpy as np
 from utilities import load_save, metrics, method_comparison
-import uuid
 import os
 from timeit import default_timer as timer
 from main import BurjKhalifaClustering
-from collections import Counter
 
 SKIP_COLUMNS_IN_DATASET = {
     ("real", "wdbc"): ["idnumber"],
-    ("real","spectrometer"): ["LRS-name"],
-    ("real","WISDM_ar_v1.1_transformed"): ["UNIQUE_ID"],
+    ("real", "spectrometer"): ["LRS-name"],
+    ("real", "WISDM_ar_v1.1_transformed"): ["UNIQUE_ID"],
 }
+
 
 def run_multiple_datasets(
     dataset_names,
@@ -20,16 +18,24 @@ def run_multiple_datasets(
     min_leaves=0,
     n_clusters=None,
 ):
-
     result_dict = {}
     for idx, dataset_name in enumerate(dataset_names):
         try:
-            print(dataset_name)
             start = timer()
-            skip_columns = [] if dataset_name not in SKIP_COLUMNS_IN_DATASET else SKIP_COLUMNS_IN_DATASET[dataset_name]
+            skip_columns = (
+                []
+                if dataset_name not in SKIP_COLUMNS_IN_DATASET
+                else SKIP_COLUMNS_IN_DATASET[dataset_name]
+            )
             df = load_save.read_arff(dataset_name[0], dataset_name[1], skip_columns)
             X, true_labels = df.loc[:, df.columns != "class"], df["class"]
-            bk_model = BurjKhalifaClustering(depth=depth, chain_ratio=chain_ratio, parent_split_ratio=parent_split_ratio,min_leaves=min_leaves, n_clusters =n_clusters)
+            bk_model = BurjKhalifaClustering(
+                depth=depth,
+                chain_ratio=chain_ratio,
+                parent_split_ratio=parent_split_ratio,
+                min_leaves=min_leaves,
+                n_clusters=n_clusters,
+            )
             bk_model.fit(X)
             predict_labels = bk_model.labels_
             error_results = metrics.calculate_metrics(true_labels, predict_labels)
@@ -37,9 +43,10 @@ def run_multiple_datasets(
                 error_results, timer() - start
             )
         except Exception as e:
-            print (e)
+            print(e)
 
     return result_dict
+
 
 def run_batch():
     skip_datasets = [
@@ -60,6 +67,7 @@ def run_batch():
         "Autism_Data",
         "WISDM_ar_v1.1_transformed",
         "Lymphoma",
+        "golfball",  # as 1 cluster, incorrect metric definition for clustering methods
         "Colon",  # multiple duplicated column names
         "jm1",  # gmm throws error
         "KDDTest+",  # gmm throws error
@@ -79,23 +87,26 @@ def run_batch():
     ]
 
     # run bk_clustering
-    '''
+
+    """
     results = run_multiple_datasets(dataset_names)
     filename = (
-        f"./../results/bk_clustering_results.json"  # Define the filename to save the results
+        f"./../results/bk_clustering_results st_scaler.json"  # Define the filename to save the results
     )
     load_save.save_json(results, filename)  # Save the results to a JSON file
-    '''
+    """
+
     # method_comparison.run_birch(dataset_names, number_of_clusters)
     # method_comparison.run_dbscan(dataset_names, number_of_clusters)
     # method_comparison.run_kmeans(dataset_names, number_of_clusters)
     # method_comparison.run_kmeans_mini_batch(dataset_names, number_of_clusters)
     # method_comparison.run_mean_shift(dataset_names, number_of_clusters)
     # method_comparison.run_agglomerative(dataset_names, number_of_clusters)
+    # method_comparison.run_gmm(dataset_names, number_of_clusters)
     # method_comparison.run_affinity_propagation(dataset_names, number_of_clusters)
-    #method_comparison.run_density_peak(dataset_names, number_of_clusters)
-    #method_comparison.run_optics(dataset_names, number_of_clusters)
-    #method_comparison.run_gmm(dataset_names, number_of_clusters)
+    # method_comparison.run_density_peak(dataset_names, number_of_clusters)
+    # method_comparison.run_optics(dataset_names, number_of_clusters)
+
 
 # python3 run_batch.py
 if __name__ == "__main__":
